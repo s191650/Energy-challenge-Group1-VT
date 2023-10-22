6ff1e51f27cd3e2452b7c6a78b0e2229051be470
BrandonDsl <brandon.desoleil@gmail.com>
2023-10-22 22:28:53 +0200

    Add data

--- Building_energy_signature.py
+++ Building_energy_signature.py
@@ -1,1 +1,9 @@
+from math import*
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
 
+meteo = pd.read_table('Meteo.csv',delimiter =";")
+building_geometry = pd.read_table('Building_geometry.csv',delimiter =";")
+
+print(K)
